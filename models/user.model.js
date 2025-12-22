f21eb0904aafdce3504417d9beed7781bb0664c6
OneSummerDay <artem.kraievyi@gmail.com>
2025-12-22 16:09:21 +0200

    feat: add user model and schema definition

---
+++ models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: [true, 'User Name is required'],
+        trim: true,
+        minLenghth: 2,
+        maxLength: 30,
+    },
+    email: {
+        type: String,
+        required: [true, 'User Email is required'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'Please fill a valid email address'],
+    },
+    password: {
+        type: String,
+        required: [true, 'User Password is required'],
+        minLength: 6,
+    },
+}, {
+    timestamps: true,
+});
+
+
+export const User = mongoose.model('User', userSchema);
