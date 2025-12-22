2779a4a0fca80fcd2734c6c56190b628299d1014
OneSummerDay <artem.kraievyi@gmail.com>
2025-12-22 15:13:25 +0200

    chore: import DB_URI to env.js

--- config/env.js
+++ config/env.js
@@ -1,8 +1,7 @@
 import { config } from 'dotenv';
 
-// eslint-disable-next-line no-undef
+
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 
-// eslint-disable-next-line no-undef
-export const { PORT, NODE_ENV } = process.env;
+export const { PORT, NODE_ENV, DB_URI } = process.env;
