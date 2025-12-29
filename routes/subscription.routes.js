592edccb028ecfd2ea5c21b7ed745c68e4c4007c
OneSummerDay <artem.kraievyi@gmail.com>
2025-12-29 22:28:25 +0200

    feat: protect user subscriptions route and add controller

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authorize from "../middlewares/auth.middleware.js";
-import { createSubscription } from "../controllers/subscription.controller.js";
+import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
@@ -22,9 +22,7 @@ subscriptionRouter.delete('/:id', (req, res) => {
     res.send({title: 'Delete Subscription by ID'});
 });
 
-subscriptionRouter.get('/user/:userId', (req, res) => {
-    res.send({title: 'Get Subscriptions by User ID'});
-});
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 
 subscriptionRouter.put('/:id/cancel', (req, res) => {
     res.send({title: 'Cancel Subscription by ID'});
